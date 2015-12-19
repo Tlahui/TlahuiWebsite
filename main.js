a2e2fdb3b8e246e5c0ecdcc18d3834e905bfe9b9
Rick <rvcuag@hotmail.com>
2015-12-19 12:36:53 -0600

    Conekta Get Test Token

---
+++ main.js
@@ -0,0 +1,36 @@
+
+
+Conekta.setPublishableKey("key_Kzzgi62PkmaZufFM7AHFzbw");
+/* Los parámetros pueden ser un objeto de javascript, una forma de HTML o una forma de JQuery */
+var errorResponseHandler, successResponseHandler, tokenParams;
+tokenParams = {
+  "card": {
+    "number": "4242424242424242",
+    "name": "Javier Pedreiro",
+    "exp_year": "2018",
+    "exp_month": "12",
+    "cvc": "123"
+  }
+};
+
+/* Después de tener una respuesta exitosa, envía la información al servidor */
+
+successResponseHandler = function(token) {
+  console.log(token);
+ /* return $.post('/process_payment?token_id=' + token.id, function() {
+    return document.location = 'payment_succeeded';
+  });*/
+};
+
+/* Después de recibir un error */
+
+errorResponseHandler = function(error) {
+  return console.log(error.message);
+};
+
+/* Tokenizar una tarjeta en Conekta */
+function createtoken()
+
+{
+  Conekta.token.create(tokenParams, successResponseHandler, errorResponseHandler);
+}
