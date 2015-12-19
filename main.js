b5c472d1dc8fb92a06b8e28aca623c35ea52a4b3
felimargom <kastulink@hotmail.com>
2015-12-19 12:37:04 -0600

    feli-conekta-website

---
+++ main.js
@@ -0,0 +1,33 @@
+/* Los parámetros pueden ser un objeto de javascript, una forma de HTML o una forma de JQuery */
+Conekta.setPublishableKey("key_Kzzgi62PkmaZufFM7AHFzbw");
+var errorResponseHandler, successResponseHandler, tokenParams;
+tokenParams = {
+  "card": {
+    "number": "4242424242424242",
+    "name": "Javier Pedreiro",
+    "exp_year": "2016",
+    "exp_month": "12",
+    "cvc": "123"
+  }
+};
+
+/* Después de tener una respuesta exitosa, envía la información al servidor */
+
+successResponseHandler = function(token) {
+  console.log(token);
+//  return $.post('/process_payment?token_id=' + token.id, function() {
+  //  return document.location = 'payment_succeeded';
+  //});
+};
+
+/* Después de recibir un error */
+
+errorResponseHandler = function(error) {
+  return console.log(error.message);
+};
+
+/* Tokenizar una tarjeta en Conekta */
+function createtoken()
+{
+  Conekta.token.create(tokenParams, successResponseHandler, errorResponseHandler);
+}
